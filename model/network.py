5c5658ab202407fb595d2b88001721846005a3f1
vikasmech <vikasmech.nitk@gmail.com>
2024-12-04 16:52:35 +0530

    model fix

--- model/network.py
+++ model/network.py
@@ -6,15 +6,15 @@ class SimpleCNN(nn.Module):
         super(SimpleCNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(10, 10, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(10 * 7 * 7, 10)
-        self.fc2 = nn.Linear(10, 10)
+        self.fc1 = nn.Linear(10 * 7 * 7, 50)
+        self.fc2 = nn.Linear(50, 10)
         self.pool = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
-        x = x.view(-1, 32 * 7 * 7)
+        x = x.view(-1, 10 * 7 * 7)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x 
